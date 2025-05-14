ceadfa1a5f821ad84925fb323e31ab24c586681b
tanshurou <tanshurou88@gmail.com>
2025-05-14 14:25:06 +0800

    pics

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -10,6 +10,6 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('History'));
+    return Center(child: Text('History Page'));
   }
 }
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:splitpal/pages/login_page.dart';
 
 class LandingPage extends StatelessWidget {
@@ -7,21 +8,36 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Landing Page'),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset('images/logo.png', height: 70),
+            ),
+            Text('SplitPal', style: TextStyle()),
 
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()),
-                );
-              },
-              child: Text('Start'),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink[300],
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                },
+                child: Text(
+                  'Get Started',
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(fontSize: 10, color: Colors.white),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
